2d9fa86f2aae536e30ac1717cfae07dd002c8de7
marceaudavid <marceau.david@ynov.com>
2019-03-12 11:42:51 +0100

    House model

---
+++ models/house.js
@@ -0,0 +1,41 @@
+const { mongoose } = require("../database/db");
+
+const HouseSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: Number,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    sigil: {
+      type: String,
+      required: true
+    },
+    words: {
+      type: String,
+      required: true
+    },
+    seat: {
+      type: String,
+      required: true
+    },
+    region: {
+      type: String,
+      required: true
+    },
+    vassals: {
+      type: Array
+    },
+    founder: {
+      type: String
+    }
+  },
+  { versionKey: false }
+);
+
+const House = mongoose.model("Character", HouseSchema);
+
+module.exports = House;
