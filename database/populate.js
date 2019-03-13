772da7aebb060da4a9730672f81304035ed4a5dd
marceaudavid <marceau.david@ynov.com>
2019-03-13 16:26:32 +0100

    Fix places import and data

--- database/populate.js
+++ database/populate.js
@@ -5,23 +5,7 @@ const { mongoose } = require("./db");
 
 const Character = require("../models/character");
 const House = require("../models/house");
-const Place = require("../models/house");
-
-fs.readFile("./database/characters.json", (error, data) => {
-  if (error) console.log(error);
-  db.connect();
-  Character.insertMany(JSON.parse(data))
-    .then(char => console.log(char))
-    .catch(err => console.log(err));
-});
-
-fs.readFile("./database/houses.json", (error, data) => {
-  if (error) console.log(error);
-  db.connect();
-  House.insertMany(JSON.parse(data))
-    .then(house => console.log(house))
-    .catch(err => console.log(err));
-});
+const Place = require("../models/place");
 
 fs.readFile("./database/places.json", (error, data) => {
   if (error) console.log(error);
