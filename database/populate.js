37293a1bd72f385ead8de92c77f83e3eaf70431b
marceaudavid <marceau.david@ynov.com>
2019-03-13 14:42:41 +0100

    Houses script and data

--- database/populate.js
+++ database/populate.js
@@ -2,9 +2,19 @@ const { mongoose } = require("./db");
 const db = require("./db");
 const fs = require("fs");
 const Character = require("../models/character");
+const Houses = require("../models/house");
 
 fs.readFile("./database/characters.json", (error, data) => {
   if (error) console.log(error);
   db.connect();
-  Character.insertMany(JSON.parse(data)).then(char => console.log(char));
+  Character.insertMany(JSON.parse(data))
+    .then(char => console.log(char))
+    .catch(err => console.log(err));
+  fs.readFile("./database/houses.json", (error, data) => {
+    if (error) console.log(error);
+    db.connect();
+    Houses.insertMany(JSON.parse(data))
+      .then(house => console.log(house))
+      .catch(err => console.log(err));
+  });
 });
