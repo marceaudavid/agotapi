28c754c14c398daf89a221a9b67eb71bde7a498d
Speleize <alexandre.suchot@ynov.com>
2019-03-13 17:38:42 +0100

    Create data for quotes, insert in db

---
+++ models/quote.js
@@ -0,0 +1,34 @@
+const { mongoose } = require("../database/db");
+
+const QuoteSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: Number,
+      required: true
+    },
+    quote: {
+      type: String,
+      required: true
+    },
+    from: {
+      type: String,
+      required: true
+    },
+    to: {
+      type: String
+    },
+    season: {
+      type: Number,
+      required: true
+    },
+    episode: {
+      type: Number,
+      required: true
+    }
+},
+{ versionKey: false }
+);
+
+const Quote = mongoose.model("Quote", QuoteSchema);
+
+module.exports = Quote;
